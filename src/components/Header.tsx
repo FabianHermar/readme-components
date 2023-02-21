06b8c2187a166bbe3659214c1d9bacd82a503805
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-21 00:37:09 -0600

    🔁 little things changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,17 +20,17 @@ const Header = () => {
                 <div className="flex flex-col h-full lg:flex-row jusfify-between lg:items-center">
                   <ul className='px-6 pt-32 text-black font-sans font-semibold space-y-8 lg:space-y-0 md:px-12 lg:flex lg:space-x-12 lg:pt-0 font-xl'>
                     <li>
-                      <a href="#" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-[burlywood]/70 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
+                      <a href="#" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-purple-300 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
                         <span className='relative group-hover:text-gray-900 ease-in duration-200'>Inicio</span>
                       </a>
                     </li>
                     <li>
-                      <a href="#" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-[burlywood]/70 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
+                      <a href="#" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-purple-300 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
                         <span className='relative group-hover:text-gray-900 ease-in duration-200'>Componentes</span>
                       </a>
                     </li>
                     <li>
-                      <a href="https://github.com/FabianHMzz/readme-components" target="_blank" rel="noopener noreferrer" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-[burlywood]/70 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
+                      <a href="https://github.com/FabianHMzz/readme-components" target="_blank" rel="noopener noreferrer" className='group relative before:absolute before:inset-x-0 before:bottom-0 hover:underline hover:decoration-purple-300 hover:decoration-4 before:h-2 before:origin-right before:scale-x-0 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100'>
                         <span className='relative group-hover:text-gray-900 ease-in duration-200'>Github</span>
                       </a>
                     </li>
