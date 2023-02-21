2ee74b7e9f680210dffb4e64f7a83d0defda6de4
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-02-21 01:35:43 -0600

    🛠️ font size repared

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return(
     <section>
         <div className='font-sans flex justify-center items-center flex-col select-none'>
-          <h1 className='absolute drop-shadow-2xl text-3xl lg:text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 top-2/4'>README Components</h1>
-          <p className="absolute top-[57%] lg:top-[70%] font-semibold text-[13px] lg:text-2xl">Dale un toque de vida a tus archivos <code>markdown.md</code></p>
+          <h1 className='absolute drop-shadow-2xl text-3xl lg:text-8xl h-full font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 top-2/4'>README Components</h1>
+          <p className="absolute top-[54%] lg:top-[70%] font-semibold text-[13px] lg:text-2xl">Dale un toque de vida a tus archivos <code>markdown.md</code></p>
           <div className='absolute mt-8 text-2xl lg:text-3xl flex items-center justify-center gap-6 top-[59%] lg:top-3/4'>
             {
               socialMedia?.map((social, icon) => (
