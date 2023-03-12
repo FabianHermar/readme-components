d684ed25094177ef28833d9366e738970d26f038
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-12 00:32:14 -0600

    🆕 dropmenu added

--- src/Header.jsx
+++ src/Header.jsx
@@ -41,6 +41,17 @@ const Header = () => {
                         <span className='relative text-black lg:text-white ease-in duration-200'>GitHub</span>
                       </a>
                     </li>
+                    <li>
+                      <details className='group relative'>
+                        <summary className='flex cursor-pointer list-none items-center justify-between lg:text-white'>Más Opciones
+                          <svg data-hk='0-0' width='18' height='13' viewBox='0 0 22 13' fill='none' xmlns='http://www.w3.org/2000/svg' className='inline-block rotate-0 transition-transform group-open:-rotate-180 ml-2 lg:text-white'><path d='M1 1L11 11L21 1' stroke='currentColor' strokeWidth='2'></path></svg>
+                        </summary>
+                        <div className='mt-[25px] flex flex-col gap-4 text-md font-sans absolute lg:backdrop-blur lg:bg-white/50 px-6 py-2 lg:text-center lg:text-white'>
+                          <a href='https://fabianhmz.dev' target='_blank' rel='noopener noreferrer' className='flex items-center gap-4 lg:hover:underline hover:decoration-2'>Web Personal</a>
+                          <a href='https://visual-tradex.com' target='_blank' rel='noopener noreferrer' className='flex items-center gap-4 lg:hover:underline hover:decoration-2'>Web Empresarial</a>
+                        </div>
+                      </details>
+                    </li>
                   </ul>
                 </div>
               </div>
