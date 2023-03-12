d27dc2d63bae1611a3338d3f49a216595bc8cd59
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-03-12 00:32:01 -0600

    🔁 little things changed

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className='font-sans'>
       <div className='relative flex flex-row justify-between items-center px-6'>
         <div>
-          <h1 className='text-3xl font-bold'>¿Qué son los README Components?</h1>
+          <h1 className='text-3xl font-bold text-white'>¿Qué son los README Components?</h1>
         </div>
       </div>
     </section>
