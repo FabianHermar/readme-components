9d123d29956823ab35184cb014e2669569e2a95a
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-04-08 00:29:01 -0600

    🆕 new information added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,16 @@
-const Home = () => {
+export default function Home() {
   return (
     <section className='font-sans'>
-      <div className='relative flex flex-row justify-between items-center px-6'>
-        <div>
-          <h1 className='text-3xl font-bold text-white'>¿Qué son los README Components?</h1>
-        </div>
+      <div className='relative flex flex-col md:flex-row justify-between items-center px-16'>
+        <h2 className='text-white text-2xl pb-4 md:pb-0 text-center md:text-left lg:text-5xl font-bold'>
+          ¿Qué son los README Components?
+        </h2>
+        <p className='text-lg lg:text-3xl text-center md:text-left  text-white/80 font-sans'>
+          Se trata de una compilación de diferentes componentes que puedes
+          agregar a tus archivos readme, desde estáticos hasta dinámicos para
+          darles un toque de vida.
+        </p>
       </div>
     </section>
   )
 }
-
-export default Home
